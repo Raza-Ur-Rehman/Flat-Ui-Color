d9831a45f1bae4e4113bfb11948fd031ab5f2cc5
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-27 01:28:48 +0500

    bug not fixed

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let color = '1234567890abcdef';
-let pickerColor = [];
-let mainBox = document.querySelector('.box')
-for (let i = 0; i <=11; i++) {
-    mainBox.innerHTML
-    
+let pickerColor = ['#'];
+let mainBox = document.querySelector('.mainbox')
+let pelattesbox = mainBox.innerHTML = `<div class="color"></div>`;
+function colorGen() {
+    for (let i = 0; i <= 3; i++) {
+        pickerColor.push += color[Math.floor(Math.random() * color.length)];
+        
+    }
+    console.log(pickerColor);
 }
+
+// pelattesbox.style.backgroundColor = `${'#' + pickerColor}` ;
+
