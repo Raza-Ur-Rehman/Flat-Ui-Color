95d55922b16c0553914c800e4d580280029b1560
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-05-31 03:07:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
+let colorBox = document.querySelector('.color-container');
+let mainBox = document.querySelector('.mainbox');
 let color = '1234567890abcdef';
-let pickerColor = ['#'];
-let mainBox = document.querySelector('.mainbox')
-let pelattesbox = mainBox.innerHTML = `<div class="color"></div>`;
-function colorGen() {
-    for (let i = 0; i <= 3; i++) {
-        pickerColor.push += color[Math.floor(Math.random() * color.length)];
-        
-    }
-    console.log(pickerColor);
+let pickerColor = '';
+// let pelattesbox = document.querySelector('.box')
+for (let i = 0; i <= 11; i++) {
+    mainBox.innerHTML += `<div class="box"><div class="innerbox"></div><h3 class="innertitle">lorem</h3></div>`;
+}
+for (let i = 0; i <= 11; i++) {
+    colorBox.innerHTML += `<div class="colorbox"><button class="copybtn">lorem</button></div>`;
 }
 
-// pelattesbox.style.backgroundColor = `${'#' + pickerColor}` ;
+
+console.log(mainBox);
+
+function colorGen() {
+    pickerColor = "#"
+    for (let i = 0; i <= 19; i++) {
+        pickerColor += color[Math.floor(Math.random() * color.length)];
+    }
+}
+
+// pelattesbox.style.backgroundColor = `${pickerColor}` ;
 
