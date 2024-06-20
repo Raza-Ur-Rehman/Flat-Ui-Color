e458715c862d628b3db37549cb8a3e9c55fc9609
Raza ur Rehman <mrazaurrehman12345@gmail.com>
2024-06-20 13:48:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 let colorBox = document.querySelector('.color-container');
 let mainBox = document.querySelector('.mainbox');
 let color = '1234567890abcdef';
-let pickerColor;
+let pickerColor = [];
 // let pelattesbox = document.querySelector('.box')
+function colorGen() {
+    // pickerColor = "#"
+    for (let i = 0; i <= 3; i++) {
+        pickerColor.push(pickerColor += color[Math.floor(Math.random() * color.length)]) ;
+    }
+}
 for (let i = 0; i <= 11; i++) {
-    mainBox.innerHTML += `<div class="box"><div class="innerbox"></div><h3 class="innertitle">lorem</h3></div>`;
+    mainBox.innerHTML += `<div class="box" onclick="colorGen();"><div class="innerbox"></div><h3 class="innertitle">lorem</h3></div>`;
 }
 for (let i = 0; i <= 19; i++) {
-    colorBox.innerHTML += `<div class="colorbox${i}" data-aos="fade-left" 
-    data-aos-anchor="#example-anchor"
-    data-aos-offset="500" 
-    data-aos-duration="500"><div><button onclick="copyColor();" class="copybtn">COPY</button></div></div>
+    colorBox.innerHTML += `<div class="colorbox${i}" onclick="colorGen();"><div><button onclick="copyColor();" class="copybtn">COPY</button></div></div>
     `;
 }
-console.log(colorBox);
-pickerColor = "#"
-for (let i = 0; i <= 3; i++) {
-    pickerColor += color[Math.floor(Math.random() * color.length)];
-}
+// console.log(colorGen());
+colorGen();
 
-function colorGen() {
-}
+
 
 function copyColor() {
     navigator.clipboard.writeText(pickerColor);
     alert("copy hogaya")
 }
-colorGen();
 // pelattesbox.style.backgroundColor = `${pickerColor}` ;
 
